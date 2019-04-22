3c28e0d4fddda648895a6513f42545764ec5f624
Leejiaxing <2466526388@qq.com>
2019-04-22 16:50:53 +0800

    修改服务器4

--- src/main/java/cn/cp/webserver/MyController.java
+++ src/main/java/cn/cp/webserver/MyController.java
@@ -1,8 +1,15 @@
 package cn.cp.webserver;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
 
+import static cn.cp.Util.UnZipFile.unZipFiles;
 
 /**
  * 测试控制器
@@ -11,36 +18,41 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2018-05-08-下午 16:46
  */
 @RestController
-public class MyController {
+public class MyController implements Serializable {
     String [] paths;
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello Spring Bootcddddddd!";
+
+    @PostMapping("/upload")
+    public Object upload(@RequestParam("file") MultipartFile fileUpload){
+        //获取文件名
+        String fileName = fileUpload.getOriginalFilename();
+        //指定本地文件夹存储图片
+        String filePath = "/Users/lijiaxing/Downloads/junit/";
+        try {
+            //将图片保存到static文件夹里
+            fileUpload.transferTo(new File(filePath+fileName));
+           // unZipFiles(new File(filePath+fileName));
+
+            return "success to upload";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "fail to upload";
+        }
     }
 
     @RequestMapping("/picUpload")
     public String picUpload(){
-        return "index";
+        return "picUpload";
     }
 
     @RequestMapping("/launch")
     public MetricJson getM() throws Exception {
         MetricJson m=new MetricJson();
         paths=new String[]{
-            "E:\\IDEAProject\\demo\\ZXing\\zxing-zxing-3.0.0",
-            "E:\\IDEAProject\\demo\\ZXing\\zxing-zxing-3.1.0",
+                "/Users/lijiaxing/Downloads/junit/junit4-r4.11",
+                "/Users/lijiaxing/Downloads/junit/junit4-r4.12"
         };
         m.calculateMetrics(paths);
-//        Data.VersionInfo.Basic b=new Data.VersionInfo.Basic("loc",11);
-//        ArrayList<Data.VersionInfo.Basic> b1=new ArrayList<>();
-//        b1.add(b);
-//        String [] h={"className","type","changeValue","changeType","dit","noc","wmc","cbo","cbo",
-//                "lcom","rfc","nom","nopm","nosm","nof","nopf","nosf","nosi","loc"};
-//        ArrayList<ArrayList<String >> h1=new ArrayList<>();
-//        m.data.versionInfo.add(new Data.VersionInfo("junit","4.8",b1,new Data.VersionInfo.Metric(h,h1)));
-
         return m;
     }
 
 }
-
